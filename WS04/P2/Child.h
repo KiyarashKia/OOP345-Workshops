40661d57d20e7aea0fa4d02f8929c34390d35af4
Kiyarash Kia <kiarashkia1999@gmail.com>
2024-02-11 23:25:04 -0500

    WS4 P2 Done / Headers Added and Submission ready

--- WS04/P2/Child.h
+++ WS04/P2/Child.h
@@ -1,3 +1,21 @@
+/***********************************************************************
+// Workshop 4 Part 2
+// Required Modules
+// File:
+// Version 1.0
+// Author
+// Description
+//	Name      : Kiarash Kia
+//	Student ID: 108688235
+//	Email     : kkia2@myseneca.ca
+//
+//	I have done all the coding by myself and only copied the code
+//	 that my professor provided to complete my workshops and
+//	 assignments.
+// Revision History
+// -----------------------------------------------------------
+// Name                 Date            Reason
+***********************************************************************/
 #pragma once
 #ifndef SENECA_CHILD_H
 #define SENECA_CHILD_H
