d02777507114efdc6adaafcaf6ecce2789569544
Kiyarash Kia <kiarashkia1999@gmail.com>
2024-02-18 18:45:02 -0500

    WS5 P1 Fixed Formatting of ostream

--- WS05/P1/Book.cpp
+++ WS05/P1/Book.cpp
@@ -12,16 +12,22 @@ namespace seneca {
 		//Extract Author
 		end = strBook.find(',', start);
 		author = strBook.substr(start, (end - start));
+		author.erase(0, author.find_first_not_of(' '));
+		author.erase(author.find_last_not_of(' ') + 1);
 
 		// Extrat Title
 		start = end + 1;
 		end = strBook.find(',', start);
 		bookTitle = strBook.substr(start, (end - start));
+		bookTitle.erase(0, bookTitle.find_first_not_of(' '));
+		bookTitle.erase(bookTitle.find_last_not_of(' ') + 1);
 
 		// Extract Country 
 		start = end + 1;
 		end = strBook.find(',', start);
 		pubCountry = strBook.substr(start, (end - start));
+		pubCountry.erase(0, pubCountry.find_first_not_of(' '));
+		pubCountry.erase(pubCountry.find_last_not_of(' ') + 1);
 
 		// Extract Price
 		start = end + 1;
@@ -36,6 +42,8 @@ namespace seneca {
 		// Extract Description
 		start = end + 1;
 		desc = strBook.substr(start);
+		desc.erase(0, desc.find_first_not_of(' '));
+		desc.erase(desc.find_last_not_of(' ') + 1);
 	}
 
 	const std::string& Book::title() const { return bookTitle; }
@@ -48,9 +56,9 @@ namespace seneca {
 
 
 	std::ostream& operator<<(std::ostream& os, const Book& book) {
-		os << std::left << std::setw(20) << book.author << " | "
+		os << std::right << std::setw(20) << book.author << " | "
 			<< std::setw(22) << book.bookTitle << " | "
-			<< std::setw(5) << book.pubCountry << " | "
+			<< std::setw(5) << std::right<< book.pubCountry << " | "
 			<< std::setw(4) << book.pubYear << " | "
 			<< std::fixed << std::setprecision(2) << std::setw(6) << book.bookPrice << " | "
 			<< book.desc;
