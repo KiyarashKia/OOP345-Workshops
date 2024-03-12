3cfb643f1134a7a081b74e50302ac60e569cccea
Kiarash <kkia2@myseneca.ca>
2024-03-12 14:33:57 -0400

    P2 Created!

---
+++ WS06/P2/Utilities.h
@@ -0,0 +1,15 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#include <istream>
+#include "Person.h"
+#include "Employee.h"
+
+
+namespace seneca {
+	Person* buildInstance(std::istream& in);
+
+}
+
+
+#endif
