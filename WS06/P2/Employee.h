e4de74b7b7a58c8fea6b2e224849097490d69a44
Kiarash <kkia2@myseneca.ca>
2024-03-12 20:44:42 -0400

    P2 Done! Ready for submission

--- WS06/P2/Employee.h
+++ WS06/P2/Employee.h
@@ -1,3 +1,20 @@
+/***********************************************************************
+// Workshop 6 Part 2
+// Version 1.0
+// Author
+// Description
+//	Name      : Kiarash Kia
+//	Student ID: 108688235
+//	Email     : kkia2@myseneca.ca
+//	Submitted 1 day late referred to the accommodation letter
+//
+//	I have done all the coding by myself and only copied the code
+//	 that my professor provided to complete my workshops and
+//	 assignments.
+// Revision History
+// -----------------------------------------------------------
+// Name                 Date            Reason
+***********************************************************************/
 #ifndef SENECA_EMPLOYEE_H
 #define SENECA_EMPLOYEE_H
 #include <iostream>
