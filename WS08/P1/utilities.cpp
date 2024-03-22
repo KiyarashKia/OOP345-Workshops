9193370b083f2a3177c644de4e5aa971759e38e5
Kiarash <kkia2@myseneca.ca>
2024-03-21 22:45:30 -0400

    P1 test

---
+++ WS08/P1/utilities.cpp
@@ -0,0 +1,29 @@
+#include <memory>
+#include "list.h"
+#include "element.h"
+#include "utilities.h"
+
+using namespace std;
+
+namespace seneca {
+    List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price) {
+        List<Product> priceList;
+
+        // TODO: Add your code here to build a list of products
+        //         using raw pointers
+        for (auto i = 0u; i < desc.size(); ++i) {
+            for (auto j = 0u; j < price.size(); ++j) {
+                if (desc[i].code == price[j].code) {
+                    Product* temp = new Product(desc[i].desc, price[j].price);
+                    temp->validate();
+                    priceList += temp;
+
+                    delete temp;
+                    temp = nullptr;
+                }
+            }
+        }
+
+        return priceList;
+    }
+}
