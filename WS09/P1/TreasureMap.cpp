3cd91170c475f98688036e889fdb35a7b7f2a681
Kiarash <kkia2@myseneca.ca>
2024-03-29 14:07:20 -0400

    WS9 P1 Done! v1.3

--- WS09/P1/TreasureMap.cpp
+++ WS09/P1/TreasureMap.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "TreasureMap.h"
 
+
 namespace seneca{
 
     size_t digForTreasure(const std::string& str, char mark){
@@ -48,55 +49,54 @@ namespace seneca{
         return os;
     }
 
-    void TreasureMap::enscribe(const char* filename) {
+    void TreasureMap::enscribe(const char* filename){
+        // Binary write
+
         std::ofstream outFile(filename, std::ios::binary);
         if (!outFile) throw std::string(filename) + " cannot be opened";
 
-        if (map) {
-            uint32_t rows32 = static_cast<uint32_t>(rows);
-            uint32_t colSize32 = static_cast<uint32_t>(colSize);
-
-            outFile.write(reinterpret_cast<const char*>(&rows32), sizeof(rows32));
-            outFile.write(reinterpret_cast<const char*>(&colSize32), sizeof(colSize32));
-
-            for (size_t i = 0; i < rows; ++i) {
-                uint32_t length = static_cast<uint32_t>(map[i].length());
+        if (map){ 
+          outFile.write(reinterpret_cast<const char*>(&rows), sizeof(rows));
+          outFile.write(reinterpret_cast<const char*>(&colSize), sizeof(colSize));
+            for (int i = 0; i < rows; ++i)
+            {
+                int length = map[i].length();
                 outFile.write(reinterpret_cast<const char*>(&length), sizeof(length));
                 outFile.write(map[i].c_str(), length);
+
             }
         }
-        else {
+        else{
             throw std::string("Treasure map is empty!");
         }
+
     }
 
-    void TreasureMap::recall(const char* filename) {
+    void TreasureMap::recall(const char* filename){ clear();
         std::ifstream inFile(filename, std::ios::binary);
         if (!inFile) throw std::string(filename) + " cannot be opened";
 
         clear();
 
-        uint32_t rows32, colSize32;
-        inFile.read(reinterpret_cast<char*>(&rows32), sizeof(rows32));
-        inFile.read(reinterpret_cast<char*>(&colSize32), sizeof(colSize32));
-
-        rows = rows32;
-        colSize = colSize32;
-
+        inFile.read(reinterpret_cast<char*>(&rows), sizeof(rows));
+        inFile.read(reinterpret_cast<char*>(&colSize), sizeof(colSize));
+    
         map = new std::string[rows];
-        for (size_t i = 0; i < rows; ++i) {
-            uint32_t length;
+        for (int i = 0; i < rows; ++i)
+        {
+            int length{};
+
             inFile.read(reinterpret_cast<char*>(&length), sizeof(length));
 
             char* buffer = new char[length + 1];
             inFile.read(buffer, length);
 
             buffer[length] = '\0';
-            map[i] = std::string(buffer);
+            map[i] = std::string(buffer, length);
             delete[] buffer;
         }
-    }
 
+    }
 
     void TreasureMap::clear(){
         delete [] map;
